fee0ad88e3c23dc021a78e4fb8444ec3835bb299
kiki0501-cell <qkrvlfflq0501@gmail.com>
2026-07-18 17:41:41 +0900

    Update TownScene.js

--- scenes/TownScene.js
+++ scenes/TownScene.js
@@ -7,33 +7,89 @@ export default class TownScene extends Phaser.Scene {
 
     create(){
 
-        const centerX = this.scale.width / 2;
-        const centerY = this.scale.height / 2;
+        const w = this.scale.width;
+        const h = this.scale.height;
 
 
+        // 배경
+        this.cameras.main.setBackgroundColor("#3a7d44");
+
+
+        // 마을 이름
         this.add.text(
-            centerX,
-            80,
+            w / 2,
+            60,
             "새벽마을",
             {
                 fontSize:"48px",
-                color:"#ffffff"
+                color:"#ffffff",
+                fontFamily:"Jua"
             }
         ).setOrigin(0.5);
 
 
+
+        // 집
+        this.add.rectangle(
+            w / 2,
+            220,
+            120,
+            100,
+            0xb5651d
+        );
+
+
+        this.add.text(
+            w / 2,
+            220,
+            "🏠",
+            {
+                fontSize:"70px"
+            }
+        ).setOrigin(0.5);
+
+
+
+        // 나무
+        this.createTree(60,200);
+        this.createTree(w-60,200);
+        this.createTree(80,450);
+        this.createTree(w-80,450);
+
+
+
+        // 플레이어
         this.player = this.add.rectangle(
-            centerX,
-            centerY,
+            w / 2,
+            h / 2,
             32,
             32,
             0x3366ff
         );
 
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cursors =
+        this.input.keyboard.createCursorKeys();
+
 
     }
+
+
+
+    createTree(x,y){
+
+        this.add.text(
+            x,
+            y,
+            "🌲",
+            {
+                fontSize:"60px"
+            }
+        ).setOrigin(0.5);
+
+    }
+
 
 
     update(){
@@ -45,20 +101,37 @@ export default class TownScene extends Phaser.Scene {
             this.player.x -= speed;
         }
 
-
         if(this.cursors.right.isDown){
             this.player.x += speed;
         }
-
 
         if(this.cursors.up.isDown){
             this.player.y -= speed;
         }
 
-
         if(this.cursors.down.isDown){
             this.player.y += speed;
         }
+
+
+
+        // 화면 밖 못 나가게
+
+        this.player.x =
+        Phaser.Math.Clamp(
+            this.player.x,
+            16,
+            this.scale.width-16
+        );
+
+
+        this.player.y =
+        Phaser.Math.Clamp(
+            this.player.y,
+            16,
+            this.scale.height-16
+        );
+
 
     }
 
