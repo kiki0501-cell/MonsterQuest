3527ca9af17065cc03d546ccccd213116aede22f
kiki0501-cell <qkrvlfflq0501@gmail.com>
2026-07-18 16:54:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
+import BootScene from "./scenes/BootScene.js";
+import MenuScene from "./scenes/MenuScene.js";
+import VillageScene from "./scenes/VillageScene.js";
 
+
+const config = {
+
+    type: Phaser.AUTO,
+
+    parent: "game",
+
+    width: 360,
+
+    height: 640,
+
+    pixelArt: true,
+
+
+    scale: {
+
+        mode: Phaser.Scale.FIT,
+
+        autoCenter: Phaser.Scale.CENTER_BOTH
+
+    },
+
+
+    physics: {
+
+        default: "arcade",
+
+        arcade: {
+
+            debug: false
+
+        }
+
+    },
+
+
+    scene: [
+
+        BootScene,
+
+        MenuScene,
+
+        VillageScene
+
+    ]
+
+};
+
+
+new Phaser.Game(config);
