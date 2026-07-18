8f0a968133c394decb458f30bd59c59117766b2c
kiki0501-cell <qkrvlfflq0501@gmail.com>
2026-07-18 17:05:45 +0900

    Update BootScene.js

--- scenes/BootScene.js
+++ scenes/BootScene.js
@@ -1,1 +1,19 @@
+export default class BootScene extends Phaser.Scene {
 
+    constructor(){
+        super("BootScene");
+    }
+
+
+    preload(){
+
+    }
+
+
+    create(){
+
+        this.scene.start("MenuScene");
+
+    }
+
+}
