fef20d7de4dc883dba9853b5a287ae405dc0dd3c
kiki0501-cell <qkrvlfflq0501@gmail.com>
2026-07-18 16:09:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function newGame(){
-    alert("Monster Quest 개발 시작!");
+function newGame() {
+
+    document.getElementById("titleScreen").classList.add("hidden");
+
+    document.getElementById("nameScreen").classList.remove("hidden");
+
+}
+
+function startAdventure() {
+
+    const name = document.getElementById("playerName").value;
+
+    if(name.trim() === ""){
+        alert("이름을 입력해주세요!");
+        return;
+    }
+
+    alert(name + "의 모험이 시작됩니다!");
+
 }
